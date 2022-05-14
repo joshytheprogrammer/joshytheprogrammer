eea740ac0aeb96e40bd811193879e2f49e390226
joshy111 <jurassicjoshua@protonmail.com>
2022-05-14 21:49:46 +0100

    added device manager

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/style-resources',
+    '@nuxtjs/device',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
