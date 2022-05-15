f10e1ca37a28d02b2b9333df2e5ececee53f24cd
joshy111 <jurassicjoshua@protonmail.com>
2022-05-15 12:41:36 +0100

    created navbar for desktop

--- components/App/NavBar.vue
+++ components/App/NavBar.vue
@@ -1,16 +1,56 @@
 <template>
   <nav>
-    <div class="logo">This is desktop</div>
-
+    <div class="logo" @click="$router.push('/')">joshytheprogrammer</div>
+    <div class="_nav">
+      <NuxtLink class="link" to="/">About</NuxtLink>
+      <NuxtLink class="link" to="/work">Work</NuxtLink>
+      <NuxtLink class="link" to="/projects">Projects</NuxtLink>
+      <NuxtLink class="link" to="/challenges">Challenges</NuxtLink>
+      <NuxtLink class="link" to="/blog">Blog</NuxtLink>
+    </div>
+    <div class="social">
+      <Icon class="icon" icon="akar-icons:github-fill" />
+      <Icon class="icon" icon="akar-icons:twitter-fill" />
+    </div>
   </nav>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue2';
 export default {
-
+  components: {
+		Icon,
+	},
 }
 </script>
 
 <style lang="scss" scoped>
+nav {
+  padding: 1rem;
+  display: inline-flex;
+  align-content: stretch;
+  justify-content: space-between;
+  align-items: baseline;
 
+  .logo {
+    @include header;
+    cursor: pointer;
+  }
+
+  ._nav {
+    .link {
+      @include nav-link;
+      margin: 0 0.4rem;
+    }
+  }
+
+  .social {
+    margin: 12px 0 0 0;
+
+    .icon {
+      cursor: pointer;
+      margin: 0 0.4rem;
+    }
+  }
+}
 </style>
