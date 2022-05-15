03c0c2df71aadd842b568cb401d8449fac44e211
joshy111 <jurassicjoshua@protonmail.com>
2022-05-15 09:39:36 +0100

    Added Icon Package @iconify/vue2

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <Navigation />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Navigation from "../components/App/NavBar.vue"
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+}
+</style>
