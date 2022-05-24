7dde79020704dc1450c42320f1325b6bc259c61e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-05-24 08:59:16 +0100

    made card reusable

--- components/About/Work.vue
+++ components/About/Work.vue
@@ -2,28 +2,46 @@
   <div class="work">
     <Heading title="Work" />
     <div class="content">
-      <div class="card"> 
-        <div class="s">
-          <img class="_img" src="https://images.unsplash.com/photo-1648737154448-ccf0cafae1c2" alt="project name" loading="lazy">
-          <div class="_body">
-            <h5 class="_title">Full Gospel Official Website</h5>
-            <p class="_text">The official FGBMFI website repo. Built with the Nuxtjs Framework.</p>
-            <div class="_actions">
-              <a href="#" target="_blank" class="preview">Preview</a>
-              <a href="#" target="_blank" class="github">GitHub Repo</a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Card v-for="item in data" :key="item.id" :data="item" />
     </div>
   </div>
 </template>
 
 <script>
 import Heading from "../Helpers/Headers.vue"
+import Card from "../Helpers/Card.vue"
 export default {
   components: {
-    Heading
+    Heading,
+    Card
+  },
+  data(){
+    return {
+      data: [
+        {
+          id: 0,
+          imgLink: "https://images.unsplash.com/photo-1648737154448-ccf0cafae1c2",
+          title: "Full Gospel Official Website",
+          desc: "The official FGBMFI website repo. Built with the Nuxtjs Framework.",
+          preview: "",
+          github: {
+            exists: false,
+            link: null
+          }
+        },
+        {
+          id: 1,
+          imgLink: "https://trackit.com.ng/img/Track%20IT%20Logo%20official1.jpg",
+          title: "TRACK I.T.",
+          desc: "The official TRACKIT website and repo. ",
+          preview: "https://track-it-ng.netlify.app",
+          github: {
+            exists: true,
+            link: "https://github.com/joshytheprogrammer/trackit-website"
+          }
+        }
+      ]
+    }
   }
 }
 </script>
@@ -46,66 +64,6 @@ export default {
 
     @media screen and (max-width: $small) {
       grid-template-columns: repeat(1,1fr);
-    }
-
-    .card {
-      padding: 2px;
-      display: block;
-      border-radius: 4px;
-      margin-bottom: 12px;
-      
-      .s {
-        &:hover {
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)
-        }
-
-        ._img {
-          width: 100%;
-          max-height: 100%;
-        }
-
-        ._body {
-          display: flex;
-          flex-direction: column;
-          padding: 0.5rem;
-
-          ._title {
-            font-weight: 600;
-            font-size: 14px;
-            line-height: 17px;
-          }
-
-          ._text {
-            font-weight: 400;
-            font-size: 14px;
-            line-height: 17px;
-          }
-
-          ._actions {
-            display: inline-flex;
-            justify-content: space-between;
-            align-items: center;
-            font-size: 14px;
-
-            .preview {
-              text-decoration: none;
-              background: $dark;
-              color: $light;
-              @include jumbotron-button;
-              font-size: inherit;
-
-            }
-
-            .github {
-              text-decoration: none;
-              color: $dark;
-              &:hover {
-                text-decoration: underline;
-              }
-            } 
-          }
-        }
-      }
     }
   }
 }
---
+++ components/Helpers/Card.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="card"> 
+    <div class="s">
+      <img class="_img" :src="data.imgLink" alt="project name" loading="lazy">
+      <div class="_body">
+        <h5 class="_title">{{data.title}}</h5>
+        <p class="_text">{{data.desc}}</p>
+        <div class="_actions">
+          <a :href="data.preview" target="_blank" class="preview">Preview</a>
+          <a v-if="data.github.exists" :href="data.github.link" target="_blank" class="github">GitHub Repo</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data"]
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  padding: 2px;
+  display: block;
+  border-radius: 4px;
+  margin-bottom: 12px;
+  
+  .s {
+    &:hover {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)
+    }
+
+    ._img {
+      width: 100%;
+      max-height: 100%;
+    }
+
+    ._body {
+      display: flex;
+      flex-direction: column;
+      padding: 0.5rem;
+
+      ._title {
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 17px;
+      }
+
+      ._text {
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 17px;
+      }
+
+      ._actions {
+        display: inline-flex;
+        justify-content: space-between;
+        align-items: center;
+        font-size: 14px;
+
+        .preview {
+          text-decoration: none;
+          background: $dark;
+          color: $light;
+          @include jumbotron-button;
+          font-size: inherit;
+
+        }
+
+        .github {
+          text-decoration: none;
+          color: $dark;
+          &:hover {
+            text-decoration: underline;
+          }
+        } 
+      }
+    }
+  }
+} 
+</style>
