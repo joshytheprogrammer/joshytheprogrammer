45b04cd5cf6023163648bdeaf530bd50d7470356
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-05-26 09:31:41 +0100

    created blog components files

---
+++ pages/blog/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="blog">
+    <MJumbotron v-if="mobile" />
+    <DJumbotron v-else />
+    <!-- All other components involved -->
+    <Content />
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex"
+
+import DJumbotron from "~/components/Blog/Jumbotron.vue"
+import MJumbotron  from "~/components/Blog/MJumbotron.vue"
+import Content from "~/components/Blog/Content.vue"
+
+export default {
+  name: 'blog',
+  computed: {
+    ...mapGetters({
+      mobile: 'mobile/mobile',
+    }),
+  },
+  components: {
+    DJumbotron,
+    MJumbotron,
+    Content
+  }
+}
+</script>
