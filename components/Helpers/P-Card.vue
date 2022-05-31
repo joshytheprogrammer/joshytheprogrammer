602208953c1b3daa9b37f4625d761f226c162924
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-05-31 15:49:57 +0100

    started proejects code

---
+++ components/Helpers/P-Card.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="card"> 
+    <img class="_img" :src="data.imgLink" alt="project name" loading="lazy">
+    <div class="_body">
+      <h5 class="_title">{{data.title}}</h5>
+      <p class="_desc">{{data.desc}}</p>
+      <div class="actions">
+        <a :href="data.github.link">Read More</a>
+        <span class="_status" :class="data.status">{{data.status.message}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data"]
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 1px solid $dark;
+  border-radius: 8px;
+
+  ._img {
+    width: 100%;
+    height: 16rem;
+    object-fit: cover;
+  }
+
+  ._body {
+    display: flex;
+    flex-direction: column;
+    padding: 0.5rem;
+
+    ._title {
+      font-weight: 600;
+      font-size: 16px;
+      line-height: 17px;
+      padding: 0.2rem 0;
+    }
+
+    ._desc {
+      font-weight: 400;
+      font-size: 12px;
+      padding: 0.5rem 0;
+    }
+
+    .actions {
+      display: flex;
+      justify-content: space-between;
+      padding: 0.2rem 0;
+
+      a {
+      text-decoration: none;
+      background: $light;
+      font-size: 12px;
+      color: inherit;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+
+      ._status {
+        background: $success;
+        color: $light;
+        border-radius: 4px;
+        font-size: 12px;
+        padding: 0.1rem 0.3rem;
+      }
+    }
+  }
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,13 +1,44 @@
 <template>
-  
+  <div class="content">
+    <Card v-for="item in data" :key="item.id" :data="item" />
+  </div>
 </template>
 
 <script>
-export default {
+import Card from "~/components/Helpers/P-Card.vue"
 
+export default {
+  components: {
+    Card
+  },
+  data(){
+    return {
+      data: []
+    }
+  },
+  async fetch(){
+    let data = await this.$content('projects').fetch()
+    
+    this.data.push(...data)
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.content {
+  width: 90%;
+  margin: 0 auto;
 
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(3,minmax(0,1fr));
+
+  @media screen and (max-width: $mediumDesktop) {
+    grid-template-columns: repeat(2,1fr);
+  }
+
+  @media screen and (max-width: $small) {
+    grid-template-columns: repeat(1,1fr);
+  }
+}
 </style>
