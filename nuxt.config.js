2fb701df13fd903e5b597832a07830c43baee862
Joshua <jurassicjoshua@protonmail.com>
2023-10-18 20:26:16 +0100

    made changes: ''

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,29 +27,21 @@ export default {
     link: [{rel: "canonical", href:"https://joshytheprogrammer.com"}],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/main.css'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/style-resources',
+    '@nuxtjs/style-resources'
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    // https://go.nuxtjs.dev/content
     '@nuxt/content',
   ],
 
@@ -62,13 +54,15 @@ export default {
     hoistUseStatements: true,
   },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in',
+  },
+
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       name: 'joshytheprogrammer',
@@ -84,9 +78,7 @@ export default {
     }
   },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
